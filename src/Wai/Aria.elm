e9b942f4cff5843a60150c2519a31e654cf39efa
Austin Bookhart <ashenkster69@gmail.com>
2019-04-07 20:21:26 -0400

    Test documentation

--- src/Wai/Aria.elm
+++ src/Wai/Aria.elm
@@ -127,10 +127,9 @@ import Wai.Token as Token exposing (AutoComplete(..), Current(..), DropEffect(..
 import Wai.TriState.Help exposing (TriState(..))
 
 
-
-{- https://www.w3.org/TR/wai-aria-1.1/#state_prop_def -}
-
-
+{-| Identifies the currently active element when DOM focus is on a composite widget, textbox, group, or application.
+[aria-activedescendant](https://www.w3.org/TR/wai-aria-1.1/#aria-activedescendant)
+-}
 activeDescendant : IdReference -> Attribute msg
 activeDescendant =
     Aria.withIdReference "aria-activedescendant"
