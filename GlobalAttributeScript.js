b1123c5a6dadc99fc9f1dc5f7c086af1df76e7c5
Austin Bookhart <austin.bookhart@hannonhill.com>
2019-10-08 09:24:47 -0400

    Add comment to better explain the included JS file

--- GlobalAttributeScript.js
+++ GlobalAttributeScript.js
@@ -1,1 +1,4 @@
+//Removes the GlobalAttributes from the inherited table section on https://www.w3.org/TR/wai-aria-1.1/#role_definitions. Useful to understand what attributes are supported/inherited outside of the base set
+
+
 ["atomic", "busy", "controls", "current", "describedby", "details", "disabled", "dropeffect", "errormessage", "flowto", "grabbed", "haspopup", "hidden", "invalid", "keyshortcuts", "label", "labelledby", "live", "owns", "relevant", "roledescription"].forEach(function(attr) {document.querySelectorAll('.role-inherited a[href="#aria-' + attr + '"]').forEach(function(elem) {elem.remove()})})
